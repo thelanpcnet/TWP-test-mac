c70678aaff0bd5ce84d876a706f1dbc1f9c92503
thelanpcnet <thelanpcnet@gmail.com>
2023-03-20 16:42:05 +0700

    Day 1

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,6 +1,11 @@
 describe('Testing on homepage', () => {
 	it('Book Now form', () => {
-		cy.viewport(1920, 1080)
-		cy.visit('https://staging.theworkproject.com')
-	})
+        cy.viewport(1920, 1080)
+        cy.visit('/')
+        cy.get('.book-a-tour-btn').click()
+        cy.get('.sydney[data-country-code="au"]').click()
+        cy.get('.hong-kong[data-country-code="hk"]').click()
+        cy.get('.singapore[data-country-code="sg"]').click()
+        cy.get('input[placeholder="Preferred Locations *"]').click()
+    })
 })
